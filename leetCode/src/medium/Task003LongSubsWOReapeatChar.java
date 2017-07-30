68d1123cc0af1a0ec65919ffa842516089756384
temaEmelyan <tema.emelyan@gmalil.com>
2017-07-30 17:06:34 +1000

    solved lengthOfLongestSubstring

--- leetCode/src/medium/Task003LongSubsWOReapeatChar.java
+++ leetCode/src/medium/Task003LongSubsWOReapeatChar.java
@@ -1,5 +1,8 @@
 package medium;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /*Given a string, find the length of the longest substring without repeating characters.
 
 Examples:
@@ -12,10 +15,28 @@ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer 
 public class Task003LongSubsWOReapeatChar {
 
     public static void main(String[] args) {
+        int abcdefa = lengthOfLongestSubstring("abadba");
 
+        System.out.println(abcdefa);
     }
 
-    public int lengthOfLongestSubstring(String s) {
-        return 1;
+    public static int lengthOfLongestSubstring(String s) {
+        int result = 0;
+        int posOfTail = 0, posOfHead = 0;
+        char[] chars = s.toCharArray();
+
+        Set<Character> characterSet = new HashSet<>();
+
+        while (posOfHead < chars.length) {
+            boolean wasAdded = characterSet.add(chars[posOfHead]);
+            if (wasAdded) {
+                posOfHead += 1;
+                result = posOfHead - posOfTail > result ? posOfHead - posOfTail : result;
+            } else {
+                characterSet.remove(chars[posOfTail]);
+                posOfTail += 1;
+            }
+        }
+        return result;
     }
 }
---
+++ leetCode/src/medium/Task005LongestPalindromicSubstring.java
@@ -0,0 +1,28 @@
+package medium;
+
+/*Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example:
+
+Input: "babad"
+
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+Example:
+
+Input: "cbbd"
+
+Output: "bb"*/
+
+public class Task005LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String longestPalindrome(String s) {
+        return "";
+    }
+}
+
