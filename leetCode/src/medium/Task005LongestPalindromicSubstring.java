bc14b1f28594a7cae2850c4c8329bef6b52f71d3
temaEmelyan <tema.emelyan@gmalil.com>
2017-07-30 19:40:49 +1000

    solved longest palindrome with brute force =(

--- leetCode/src/medium/Task005LongestPalindromicSubstring.java
+++ leetCode/src/medium/Task005LongestPalindromicSubstring.java
@@ -15,14 +15,41 @@ Input: "cbbd"
 
 Output: "bb"*/
 
+
 public class Task005LongestPalindromicSubstring {
 
     public static void main(String[] args) {
+        System.out.println(longestPalindromeBruteForce("abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababaabababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababaabababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababaabababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababa"));
 
     }
 
-    public static String longestPalindrome(String s) {
-        return "";
+    private static String longestPalindromeBruteForce(String s) {
+        int left = 0, right = s.length() - 1;
+
+        String result = s.substring(0, 1);
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = s.length() - 1; j > i; j--) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    String substring = s.substring(i, j + 1);
+                    if (checkIfPalindromeForBruteForce(substring)) {
+                        if (result.length() < substring.length()) {
+                            result = substring;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private static boolean checkIfPalindromeForBruteForce(String s) {
+        StringBuilder normal = new StringBuilder(s);
+        StringBuilder reverse = new StringBuilder(s).reverse();
+        if (reverse.toString().equals(normal.toString())) {
+            return true;
+        }
+        return false;
     }
 }
 
