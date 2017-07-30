cbccfda0cd6c0ebbe14996aab415325506836d40
temaEmelyan <tema.emelyan@gmalil.com>
2017-07-30 20:11:59 +1000

    solved 141

---
+++ leetCode/src/easy/Task141LinkedListCycle.java
@@ -0,0 +1,43 @@
+package easy;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Task141LinkedListCycle {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    private static boolean hasCycle(ListNode head) {
+        ListNode turtle, hare;
+
+        turtle = hare = head;
+
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        while (true) {
+            if (hare.next == null || hare.next.next == null) {
+                return false;
+            }
+
+            hare = hare.next.next;
+            turtle = turtle.next;
+
+            if (turtle.val == hare.val) {
+                return true;
+            }
+        }
+    }
+}
